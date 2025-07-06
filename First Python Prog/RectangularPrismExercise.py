360c8d9e02a86acb4fae424c8739fb61e794289f
venkateshellur <venkatesh.ellur@gmail.com>
2025-07-06 15:37:46 -0500

    37. Rectangular Prism Exercise

---
+++ First Python Prog/RectangularPrismExercise.py
@@ -0,0 +1,5 @@
+def RectangularPrism(length=1,width=1,height=1):
+    return (length*width*height)
+
+RPV = RectangularPrism(5,5,5)
+print("The volume of RectangularPrism for provided values is : "+str(RPV))
