ba03d638769edbdeaa2c359e7dbbeb14a6f140ae
venkateshellur <venkatesh.ellur@gmail.com>
2025-07-06 13:55:45 -0500

    21. Some of the Escape Sequences
    Most escape sequences are the same like in C#

---
+++ First Python Prog/FewEscapeSequences.py
@@ -0,0 +1,11 @@
+# \t tab space
+# \n new line  
+# \' and \" to get the single and double quotes
+# \\ backslash
+
+
+print("This\tis\tExample\tof\tTab\tSpace")
+print("This\nis\nExample\nof\nnew\nline")
+print("Venky \"loves\" Deepu")
+print("Venky \'loves\' Deepu")
+print("what is the result of 20\\5 ?")
