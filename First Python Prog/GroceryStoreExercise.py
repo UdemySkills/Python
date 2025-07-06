250f9b8a484a50c03def2db6e9a68f50d5cd8fc1
venkateshellur <venkatesh.ellur@gmail.com>
2025-07-06 13:30:54 -0500

    14. GroceryStoreExercise

---
+++ First Python Prog/GroceryStoreExercise.py
@@ -0,0 +1,18 @@
+penne16Oz = 16.68
+arrabiata24Oz = 6.98
+OGC20 = 16.78
+italianSeaeoning = 15.26
+ABTP = 3.00
+MB12 = 4.39
+
+print("Approach 1 : Grand total of all the products is : "+ str(round(penne16Oz+arrabiata24Oz+OGC20+italianSeaeoning+ABTP+MB12,2)))
+
+
+arrabiata24Oz = 6.98             * 100
+penne16Oz = 16.68                * 100
+OGC20 = 16.78 * 100
+italianSeaeoning = 15.26 * 100
+ABTP = 3.00 * 100
+MB12 = 4.39 *100
+
+print("Approach 2 : Grand total of all the products is : "+ str((penne16Oz+arrabiata24Oz+OGC20+italianSeaeoning+ABTP+MB12)/100))
