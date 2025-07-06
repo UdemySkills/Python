97046a23f8f39715f238df488740c1e1237bd298
venkateshellur <venkatesh.ellur@gmail.com>
2025-07-06 13:50:09 -0500

    20. type() and str()

---
+++ First Python Prog/TypeandStringFunc.py
@@ -0,0 +1,16 @@
+boolStatus = False
+intSomeNumber = 567
+decimalSomeNumber = 5.12389
+
+
+#type function returns the datatype of the variable
+#str function converts any type to string
+print("Datatype of boolStatus is : " + str(type(boolStatus)))
+print("Datatype of intSomeNumber is : " + str(type(intSomeNumber)))
+print("Datatype of decimalSomeNumber is : " + str(type(decimalSomeNumber)))
+
+
+#converts the datatyps to string so that it can be concatinated and printed
+print("Datatype of boolStatus is : " + str(boolStatus))
+print("Datatype of intSomeNumber is : " + str(intSomeNumber))
+print("Datatype of decimalSomeNumber is : " + str(decimalSomeNumber))
