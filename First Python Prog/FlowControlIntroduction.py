0c87c0f227a3641e986185a2dd9c72a3c2f8253c
venkateshellur <venkatesh.ellur@gmail.com>
2025-07-06 17:02:52 -0500

    46. FlowControls Operators

---
+++ First Python Prog/FlowControlIntroduction.py
@@ -0,0 +1,17 @@
+#Comparision Operatiors
+
+print("is 4 > 2 ? "+str(4 > 2))
+print("is 4 >= 2 ? "+str(4 >= 2))
+print("is 4 <= 2 ? "+str(4 <= 2))
+print("is 4 != 2 ? "+str(4 != 2))
+print("Is Hello = Hello ? "+ str("Hello" == "Hello"))
+print("Is Hello = hello ? "+ str("Hello" == "hello"))
+print("Is Hello != hello ? "+ str("Hello" != "hello"))
+print("is decimal and int comparision allowed ? Yes. 4.0 != 2 ? "+str(4.0 != 2))
+
+
+
+#Boolean Operators
+print("Using and boolean operator (10 == 5 and 10 !=5) ? "+str(10 == 5 and 10 !=5))
+print("Using or boolean operator (10 == 5 and 10 !=5) ? "+str(10 == 5 or 10 !=5))
+print("Using not boolean operator (not \"Pthon\" != \"Pthon\") ? "+str(not "Pthon" != "Pthon"))
