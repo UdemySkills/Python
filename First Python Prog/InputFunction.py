dd6f0e71f5f5c257116b6fee1fe9fbff774019f5
venkateshellur <venkatesh.ellur@gmail.com>
2025-07-06 14:03:36 -0500

    26. Input function
    The input functions read any values entered to String type only and not to any other data type.

---
+++ First Python Prog/InputFunction.py
@@ -0,0 +1,8 @@
+#the input function always reads the vaues as String and not to any other datatype.
+
+name = input("Please enter your name.")
+print ("Your name is : "+name+".\n And the DataType is : "+str(type(name)))
+
+
+favNumber = input("input your favourite Number")
+print ("Your Favourite number is : "+favNumber+".\n And the DataType is : "+str(type(favNumber)))
