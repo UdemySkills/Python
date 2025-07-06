ba063fc1ffac8ee3eae4eb26ae8c261c4dd27049
venkateshellur <venkatesh.ellur@gmail.com>
2025-07-06 13:06:25 -0500

    7. Simple Math operations

--- First Python Prog/Lecture_Material.py
+++ First Python Prog/Lecture_Material.py
@@ -7,3 +7,12 @@ print("hello world")
 print ("Simple Int variable value is : "+str(ex_var))
 print("Floating value is : "+ str(floatExVar))
 print("Boolean value is : " + str(boolExVar))
+print ("Addition : " + str(5+4))
+print ("Subtraction : " + str(5-4))
+print ("Multiplication : " + str(5*4))
+print ("Division : " + str(5/4))
+print ("Addition : " + str(5+4))
+print ("Exponentiation : " + str(5**4))     #5*5*5*5
+print ("Floor Division : " + str(16//5))    #Return the wholenumber of the division result and ignores the decimal
+print ("Modulo : " + str(5%4))              #reminder of the division
+print("Python follows BODMAS")
