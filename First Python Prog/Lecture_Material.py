75cb7f35c83adc82cc1065d284fd545710279866
venkateshellur <venkatesh.ellur@gmail.com>
2025-07-06 12:44:20 -0500

    Initial Commit

---
+++ First Python Prog/Lecture_Material.py
@@ -0,0 +1,9 @@
+ex_var = 5
+ex_var = ex_var +7
+floatExVar = 4.589
+boolExVar = True
+
+print("hello world")
+print ("Simple Int variable value is : "+str(ex_var))
+print("Floating value is : "+ str(floatExVar))
+print("Boolean value is : " + str(boolExVar))
