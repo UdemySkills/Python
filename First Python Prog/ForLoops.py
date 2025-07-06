5eb54a34925fde32af1cc250574654290b2573f6
venkateshellur <venkatesh.ellur@gmail.com>
2025-07-06 18:20:45 -0500

    65. For Loops and Programming Challange

---
+++ First Python Prog/ForLoops.py
@@ -0,0 +1,13 @@
+word = "Deepika is Amazing"
+
+for char in word:
+    print(char)
+
+
+#Find number of characters in the string
+
+countOfChars=0
+for char in word:
+    countOfChars +=1
+
+print(countOfChars)
